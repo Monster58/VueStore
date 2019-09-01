5589e16dcce2c59ecd2a0a3e896356b25f6bcf08
admin <dudududadada369@gmail.com>
2019-09-02 00:05:22 +0800

    完成底部路由切换

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@
 import Vue from 'vue'
 
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 import { Header } from 'mint-ui'
+
+//导入路由
+import VueRouter from 'vue-router'
+// 安装路由
+Vue.use(VueRouter)
+import router from './router.js'
 
 //导入根组件
 import app from './App.vue'
+
+
 Vue.component(Header.name, Header)
 
 const vm = new Vue({
         el: '#app',
-        render: c => c(app)
+        render: c => c(app),
+        router
     })
     //
--- src/router.js
+++ src/router.js
@@ -1,12 +1,19 @@
 import VueRouter from 'vue-router'
 
 //导入对应路由组件
-
+import home from './components/tabbar/home.vue'
+import member from './components/tabbar/member.vue'
+import shopcar from './components/tabbar/shopcar.vue'
+import search from './components/tabbar/search.vue'
 
 
 var router = new VueRouter({
     routes: [
-
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: home },
+        { path: '/member', component: member },
+        { path: '/shopcar', component: shopcar },
+        { path: '/search', component: search }
     ],
     linkActiveClass: 'mui-active' //默认覆盖路由高亮的类默认叫做router-link-active
 })
